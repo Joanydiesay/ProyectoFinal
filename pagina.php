35c6114e769373169d9b7e0e34c800f0a5ff3408
Joanydiesay <joanydiesay15@gmail.com>
2025-11-24 01:10:33 -0500

    JYY

---
+++ pagina.php
@@ -0,0 +1,41 @@
+<pagina class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>enlaces rapidos</h3>
+         <a href="inicio.php"> <i class="fas fa-angle-right"></i> inicio</a>
+         <a href="comercio.php"> <i class="fas fa-angle-right"></i> tienda</a>
+         <a href="sobre_nosotros.php"> <i class="fas fa-angle-right"></i> sobre nosotros</a>
+         <a href="contactos.php"> <i class="fas fa-angle-right"></i> contacto</a>
+      </div>
+
+      <div class="box">
+         <h3>enlaces adicionales</h3>
+         <a href="carrito.php"> <i class="fas fa-angle-right"></i> carrito</a>
+         <a href="lista_de_deseos.php"> <i class="fas fa-angle-right"></i> lista de deseos</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> iniciar sesion</a>
+         <a href="registrar.php"> <i class="fas fa-angle-right"></i> registrarse</a>
+      </div>
+
+      <div class="box">
+         <h3>informacion de contacto</h3>
+         <p> <i class="fas fa-phone"></i> +51-902104068 </p>
+         <p> <i class="fas fa-phone"></i> +51-923828932 </p>
+         <p> <i class="fas fa-envelope"></i> joanydiesay15@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Lima, Peru - 07001 </p>
+      </div>
+
+      <div class="box">
+         <h3>siguenos</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> &copy; derechos de autor @ <?= date('Y'); ?> por <span>joanydiesay15</span> | ¡todos los derechos reservados!</p>
+
+</pagina>
